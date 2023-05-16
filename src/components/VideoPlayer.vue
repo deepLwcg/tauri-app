74232829fb8f4a7f411576a1d2011aae8cabbf7c
baiduren <2073630739@qq.com>
2023-05-16 08:26:51 +0800

    first commit

--- src/components/VideoPlayer.vue
+++ src/components/VideoPlayer.vue
@@ -1,11 +1,46 @@
 <template>
-  <div class="video-player"></div>
+  <div id="dplayer" ref="player"></div>
 </template>
+<script setup lang="ts">
+  import Hls from 'hls.js';
+  import DPlayer, { DPlayerEvents } from 'dplayer';
+  import { ref, onMounted } from 'vue';
+  import { invoke } from '@tauri-apps/api/tauri';
 
-<script setup type="ts"></script>
+  const toggle_fullscreen = () => {
+    invoke('toggle_fullscreen');
+  };
+
+  const player = ref<HTMLElement | null>(null);
+  onMounted(() => {
+    const dp = new DPlayer({
+      container: document.getElementById('dplayer'),
+      lang: 'zh-cn',
+      video: {
+        url: 'https://vip.lz-cdn17.com/20230511/12010_751767f8/index.m3u8',
+        type: 'customHls',
+        customType: {
+          customHls: function (video) {
+            const hls = new Hls();
+            hls.loadSource(video.src);
+            hls.attachMedia(video);
+          },
+        },
+      },
+    });
+    dp.on(<DPlayerEvents>'fullscreen', function () {
+      console.log('fullscreen');
+      toggle_fullscreen();
+    });
+    dp.on(<DPlayerEvents>'fullscreen_cancel', function () {
+      console.log('fullscreen_cancel');
+      toggle_fullscreen();
+    });
+  });
+</script>
 
 <style scoped>
-  .video-player {
+  #dplayer {
     width: 100%;
     height: 100%;
     background: #2c3e50;
